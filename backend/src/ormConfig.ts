0cb0b389516f9024804781962e083952816c65d7
dev4us <bronxofsoul@gmail.com>
2019-12-28 17:08:04 +0900

    # BE 4.1. set DotEnv

--- backend/src/ormConfig.ts
+++ backend/src/ormConfig.ts
@@ -1,6 +1,9 @@
 // Connection 은 나중에 추가하기
 import { ConnectionOptions, createConnection, Connection } from "typeorm";
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 // 1. type을 불러왔을 때 아래 자동완성으로 보여주는지 보여주기
 const connectionOptions: ConnectionOptions = {
   type: "postgres",
